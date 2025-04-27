2fa2f3ffd5226c12e8576c33f3390a95df70adbe
root <gauravkumarsl.a00@gmail.com>
2025-04-27 18:24:28 +0000

    table structure

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Document_Search_And_Virtual_Assitant
 RAG Chatbot for document searching and summarization
+
+Start the chatbot with following command:
+``` 
+poetry run uvicorn api.main:app --reload --host 0.0.0.0 --port 8080 
+```
---
+++ api/router/health.py
@@ -0,0 +1,10 @@
+
+from fastapi import FastAPI, APIRouter, Form
+from validation_models.schemas import health
+
+router=APIRouter()
+
+@router.post("/health")
+async def root(request:health):
+    return {"message": f"Hello {request.name}, I'm alive"}
+
